36406ffd851ded862beb74720bde51ad8bf5f418
Daniel <avelar42@hotmail.com>
2023-02-05 20:45:23 -0300

    Lista de Imagens

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key? key}) : super(key: key);
@@ -7,19 +9,29 @@ class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Meus Lugares'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
-              },
-              icon: Icon(Icons.add))
-        ],
-      ),
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Meus Lugares'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
+                },
+                icon: Icon(Icons.add))
+          ],
+        ),
+        body: Consumer<GreatPlaces>(
+          child: Center(child: Text('Nenhum local cadastrado!')),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.itemsCount == 0
+              ? ch!
+              : ListView.builder(
+                  itemCount: greatPlaces.itemsCount,
+                  itemBuilder: (ctx, i) => ListTile(
+                        leading: CircleAvatar(
+                            backgroundImage:
+                                FileImage(greatPlaces.itemByIndex(i).image)),
+                        title: Text(greatPlaces.itemByIndex(i).title),
+                        onTap: () {},
+                      )),
+        ));
   }
 }
