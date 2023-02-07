cf77d051cf64ff0d801793f0d7e58e1e08ff8e4d
Daniel <avelar42@hotmail.com>
2023-02-06 22:21:52 -0300

    Sql lite connection (get insert)

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,9 +3,23 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:great_places/models/place.dart';
+import 'package:great_places/utils/db_util.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
+
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData('places');
+    _items = dataList
+        .map((item) => Place(
+            id: item['id'],
+            title: item['title'],
+            location: null,
+            image: File(item['image'])))
+        .toList();
+    notifyListeners();
+  }
+
   List<Place> get items {
     return [..._items];
   }
@@ -29,6 +43,11 @@ class GreatPlaces with ChangeNotifier {
         location: null,
         image: image);
     _items.add(newPlace);
+    DbUtil.insert("places", {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path
+    });
     notifyListeners();
   }
 }
